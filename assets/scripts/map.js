76860a8630a5c148e9c8ce2796dedbdf837d1189
luis gomez <luisgomezojeda110502@gmail.com>
2022-12-26 17:16:52 -0300

    correcion api map

--- assets/scripts/map.js
+++ assets/scripts/map.js
@@ -1,28 +1,34 @@
 export function getMap(){
- let map = L.map('map').setView([-40.7343896,-64.941539],19);
+//  let map = L.map('map').setView([-40.7343896,-64.941539],19);
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+//     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//         maxZoom: 19,
+//         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+//     }).addTo(map);
 
-    // let polygon = L.polygon([
-    //     [51.509, -0.08],
-    //     [51.503, -0.06],
-    //     [51.51, -0.047]
-    // ]).addTo(map);
+//     // let polygon = L.polygon([
+//     //     [51.509, -0.08],
+//     //     [51.503, -0.06],
+//     //     [51.51, -0.047]
+//     // ]).addTo(map);
 
+
+    let map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
     let myIcon = L.icon({
         iconUrl: './assets/images/icon-location.svg',
-        iconAnchor: [50, 94],
-        popupAnchor: [-3, -76],
+        iconAnchor: [22, 54],
+        popupAnchor: [0, 0],
         shadowUrl: '',
-        shadowSize: [68, 95],
-        shadowAnchor: [22, 94]
+        shadowSize: [0, 0],
+        shadowAnchor: [0, 0]
     });
     
-    L.marker([-40.7343896,-64.941539], {icon: myIcon}).addTo(map);
+    L.marker([51.5, -0.09], {icon: myIcon}).addTo(map);
 
 
 }
